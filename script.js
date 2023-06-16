52e5cc9146ee296c551d28ffbf6e87c65b11f1b9
CedricSanchezGithub <mycedmail@gmail.com>
2023-06-16 09:58:41 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const image = document.querySelector('.logo');
+let isListening = true;
 
-image.addEventListener('click', () => {
+function crisLion() {
+  if (isListening) {
     const audio = new Audio('layouts/lion.mp3');
     audio.play();
-});
+    console.log("rawr");
+    isListening = false;
+
+    setTimeout(() => {
+      isListening = true;
+    }, 6000);
+  }
+}
+
+image.addEventListener('mouseenter', crisLion);
