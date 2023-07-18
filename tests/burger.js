d8447d2f883df630c78c8d5e3e4f94226dcf63fb
CedricSanchezGithub <mycedmail@gmail.com>
2023-07-18 10:52:02 +0200

    nettoyage

--- tests/burger.js
+++
@@ -1,7 +0,0 @@
-const allBoxes = document.querySelectorAll(".box");
-
-allBoxes.forEach((box) => {
-  box.addEventListener("click", (e) => {
-    e.target.classList.toggle("active");
-  });
-});
--- tests/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.animated-button').addEventListener('click', function() {
-    this.classList.add('animate');
-    
-    setTimeout(() => {
-      this.classList.remove('animate');
-    }, 700);
-  });
-  
